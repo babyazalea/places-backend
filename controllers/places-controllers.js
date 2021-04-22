c8c83de511c5d86aff7b72f2b841abe34fca41be
babyazalea <usun12@hotmail.com>
2021-04-22 13:09:19 +0900

    added express-validator

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -60,6 +61,11 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("invalid inputs passed.", 422);
+  }
   const { title, description, coordinates, address, creator } = req.body;
 
   const createdPlace = {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const { check } = require("express-validator");
 
-const placeControllers = require("../controllers/places-controller");
+const placeControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
@@ -8,7 +9,15 @@ router.get("/:pid", placeControllers.getPlaceById);
 
 router.get("/user/:uid", placeControllers.getPlacesByUserId);
 
-router.post("/", placeControllers.createPlace);
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placeControllers.createPlace
+);
 
 router.patch("/:pid", placeControllers.updatePlace);
 
