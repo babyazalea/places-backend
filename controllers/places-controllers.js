0a892c5c6c0a80989f060a549dca90394373b569
babyazalea <usun12@hotmail.com>
2021-04-27 16:00:08 +0900

    cleaning up code

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -5,7 +5,6 @@ const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
 const User = require("../models/user");
-const mongooseUniqueValidator = require("mongoose-unique-validator");
 
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
