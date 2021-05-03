d41189c4c33c0e7eaa250fcb327342b25f00a3fd
babyazalea <usun12@hotmail.com>
2021-05-03 17:03:36 +0900

    adjust port

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((error) => {
     console.log(error);
