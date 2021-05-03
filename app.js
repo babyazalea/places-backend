989cdb4b59bfa7d3ccd065c7ed31cc3a39096cf1
babyazalea <usun12@hotmail.com>
2021-05-03 15:21:13 +0900

    added new environment

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://placesAdmin:${process.env.MONGODB_PASSWORD}@cluster0.6dqmg.mongodb.net/places?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6dqmg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
