b89c3d6d762ce7dc8f96f690f9a8ab30662cedc1
babyazalea <usun12@hotmail.com>
2021-05-03 17:06:17 +0900

    re-gitignore

---
+++ util/location.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const HttpError = require("../models/http-error");
+
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+async function getCoordsForAddress(address) {
+  //   return { lat: 35.1641929, lng: 129.0643827 };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError("Could not find location.", 422);
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
